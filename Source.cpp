d16073bee0dc477c7776cafba29c18618bd79afe
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-04-20 20:22:32 +0500

    class_1

--- Source.cpp
+++ Source.cpp
@@ -1,102 +1,39 @@
-﻿/*
-Написать перегруженные функции и проте-
-стировать их в основной программе:
-■ Нахождения максимального значения в одномерном
-массиве; Нахождения максимального значения в двумерном
-массиве;
-■ Нахождения максимального значения в трёхмерном
-массиве;
-*/
-
+﻿
 #include<iostream>
-#include<stdlib.h>
+#include<string>
 #include<time.h>
 
 using namespace std;
 
-void createElements(int* pElements, int const SIZE) {
-	for (int i = 0; i < SIZE; i++) {
-		*(pElements + i) = rand() % 11 + 1;
-	}
-}
 
-void createElements(int** pElements, int const SIZE) {
-	for (int i = 0; i < SIZE; i++) {
-		*(pElements + i) = new int[SIZE];
-		createElements(*(pElements + i), SIZE);
-	}
-}
-
-
-
-void showElements(int* pElements, int const SIZE) {
-	for (int i = 0; i < SIZE; i++) {
-		cout << *(pElements + i) << " ";
-	}
-	cout << endl;
-}
-void showElements(int** pElements, int const SIZE) {
-	for (int i = 0; i < SIZE; i++) {
-		showElements(*(pElements + i), SIZE);
-	}
-}
-
-
-
-int* maxNumderElements(int* pElements, int const SIZE) {
-	int* max = pElements;
-	for (int i = 1; i < SIZE; i++) {
-		if (*(pElements + i) > *max) {
-			*max = *(pElements + i);
+class Student {
+		int age;
+		string name;
+		int num_grupp;
+	public:
+		void set_Age(int ageValue) {
+			age = ageValue;
 		}
-		return max;
-	}
-};
-
-int* maxNumderElements(int** pElements, int const SIZE) {
-	int* max = *pElements;
-	for (int i = 1; i < SIZE; i++) {
-		if (*max < *maxNumderElements(*(pElements + i), SIZE)) {
-			*max = *maxNumderElements(*(pElements + i), SIZE);
+		void set_Name(string nameValue) {
+			name = nameValue;
 		}
-	}
-	return max;
+		void set_num_grupp(int num_gruppValue) {
+			num_grupp = num_gruppValue;
+		}
+		void print() {
+			cout << " Age " << age << "\n Name " << name << "\n Number " << num_grupp << endl;
+		}
 };
-
-void deleteElements(int* pElements, int const SIZE) {
-	delete[] pElements;
-}
-
-void deleteElements(int** pElements, int const SIZE) {
-	for (int i = 0; i < SIZE; i++) {
-		deleteElements(*(pElements + i), SIZE);
-	}
-	delete[] pElements;
-}
 
 int main() {
 	setlocale(LC_ALL, "RUS");
 	
-	srand(time(NULL));
+	Student student;
 
-	int const SIZE = 10;
-
-	int* elements = new int[SIZE];
-	int** twoElements = new int*[SIZE];
-	int*** tryElements = new int** [SIZE];
-
-	// Одномерный массив
-	createElements(elements, SIZE);
-	showElements(elements, SIZE);
-	cout << "\nМаксимальный элемент: " << *maxNumderElements(elements, SIZE);
-	deleteElements(elements, SIZE);
-	
-	// Двумерный массив
-	createElements(twoElements, SIZE);
-	showElements(twoElements, SIZE);
-	cout << "\nМаксимальный элемент: " << *maxNumderElements(twoElements, SIZE);
-	deleteElements(twoElements, SIZE);
-
+	student.set_Age(15);
+	student.set_Name("ildar");
+	student.set_num_grupp(10);
+	student.print();
 
 	return 0;
 }
