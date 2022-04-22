412a1e3d7e2d496b2ccdc2cfa4d7d3fe3390277b
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-04-22 20:06:29 +0500

    new

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,14 @@
 ﻿/*
-Задание №1
+К уже реализованному классу Student добавьте необходимые
+конструкторы, сделайте ряд функций inline. Обязательно выделять память под ФИО
+студента динамически, выделенную память необходимо освобо
+ждать в деструкторе.
 Реализуйте класс “Студент”. Необходимо хранить в перемен-
 ных-членах класса: ФИО, дату рождения, контактный телефон,
 город, страну, название учебного заведения, город и страну (где
 находится учебное заведение), номер группы. Реализуйте функ-
 ции-члены класса для ввода данных, вывода данных, реализуйте
-аксессоры для доступа к отдельным переменным-членам.
+аксессоры для доступа к отдельным переменным-членам. Добавьте конструкторы
 */
 
 #include<iostream>
@@ -13,73 +16,51 @@
 
 using namespace std;
 
-/// <summary>
-/// Класс описывающий студента
-/// </summary>
 class Student {
-		string surname;
-		string name;
-		string middleТame;
-		string birthdate;
-		string telefon;
-		int numberGrupp;
-	public:
-		void setSurname(string value) {
-			surname = value;
-		}
-		void setName(string value) {
-			name = value;
-		}
-		void setMiddleТame(string value) {
-			middleТame = value;
-		}
-		void setBirthdate(string value) {
-			birthdate = value;
-		}
-		void setTelefon(string value) {
-			telefon = value;
-		}
-		void setNumberGrupp(int value) {
-			numberGrupp = value;
-		}
-		string getSurname() {
-			return surname;
-		}
-		string getName() {
-			return name;
-		}
-		string getMiddleТame() {
-			return middleТame;
-		}
-		string getBirthdate() {
-			return birthdate;
-		}
-		string getTelefon() {
-			return telefon;
-		}
-		int getberGrupp() {
-			return numberGrupp;
-		}
-		void print() {
-			cout << "Фамилия " << surname << "\nИмя " << name << "\nОтчетсво " << middleТame 
-				 << "\nДата рождения " << birthdate << "\nТелефон " << telefon << "\nНомер группы "
-				 << numberGrupp;
-		}
+	string *name;
+	string *birday;
+	string *telefon;
+	int *numderGrupp;
+public:
+	Student(string name, string birday, string telefon, int numderGrupp) {
+
+		this->name = new string[name.length()];
+		this->name = &name;
+
+		this->birday = new string[birday.length()];
+		this->birday = &birday;
+
+		this->telefon = new string[telefon.length()];
+		this->telefon = &telefon;
+
+		this->numderGrupp = &numderGrupp;
+	}
+	void print() {
+		cout << " ФИО " << name << " День рождения " << birday
+			<< " Телефон " << telefon << " Номер группы " << numderGrupp << endl;
+	}
+	~Student() {
+		cout << "Деструктор начало" << endl;
+		
+		delete this->name;
+		delete this->birday;
+		delete this->telefon;
+		delete this->name;
+
+		cout << "Деструктор конец" << endl;
+	}
+
 };
+
 
 int main() {
 	setlocale(LC_ALL, "RUS");
-	
-	Student student;
 
-	student.setSurname("Ягудин");
-	student.setName("Ильдар");
-	student.setMiddleТame("Ришатович");
-	student.setBirthdate("15/10/1986");
-	student.setTelefon("+7373263104");
-	student.setNumberGrupp(15);
+	Student student1("Ягудин", "15101986", "1234567", 55);
 
-	student.print();
+
+	student1.print();
+
 
 	return 0;
 }
